25f5a9d7d209408fc9fbaecfb8aede5cd74e4b50
EduardoCardosoAgostinetti <eduardo.c.01@hotmail.com>
2024-09-07 16:43:23 -0300

    Simple CRUD

---
+++ mysql.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'root',
+  database: 'crud'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao MySQL: ', err);
+    return;
+  }
+  console.log('Conectado ao MySQL!');
+});
+
+module.exports = connection;
